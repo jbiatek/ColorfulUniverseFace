3d728ccbe0674777f7a7a0a1ec9f45dbd9e95890
jbiatek <jbiatek@gmail.com>
2015-05-12 22:21:57 -0500

    Now builds and runs on black and white Pebbles too

--- src/main.c
+++ src/main.c
@@ -3,8 +3,8 @@
 
 #define DEBUG false
   
-#define CIRCLES 256
-#define RADIUS 10
+#define CIRCLES 512
+#define RADIUS 13
   
 #define TIME_HEIGHT 50
 #define TIME_WIDTH 144
@@ -13,11 +13,11 @@
 static Window *s_main_window; 
 static TextLayer *s_time_layer;
 static Layer *s_canvas_layer;
-static struct Animation *s_animation;
-static uint16_t s_animation_counter;
+static AnimationProgress s_animation_counter;
 
 static unsigned int s_rand_seed;
 
+#ifdef PBL_COLOR
 static GColor random_color() {
   return (GColor8) { .argb = ((rand() % 0b00111111) + 0b11000000)  };
 }
@@ -25,6 +25,7 @@ static GColor random_color() {
 static GColor invert(GColor c) {
   return (GColor8) { .argb = (0b00111111 ^ c.argb) };
 }
+#endif
 
 /*static uint16_t int_sqrt(uint16_t n) {
   uint16_t ret = n;
@@ -41,47 +42,74 @@ static void background_layer_draw(Layer *layer, GContext *ctx) {
   srand(s_rand_seed);
   GRect bounds = layer_get_bounds(layer);
 
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), 0, GCornerNone);
+  
+  int16_t width = bounds.size.w * 3/2;
+  int16_t height = bounds.size.h * 3/2;
+
   // Make a whole bunch of random color circles and lines
   size_t i;
   for (i = 0; i < CIRCLES; i++) {
+    #ifdef PBL_COLOR
     GColor color = random_color();
+    #else
+    GColor color = GColorWhite;
+    #endif
     //GColor color = GColorLightGray;
     if (rand() % 2) {
       graphics_context_set_fill_color(ctx, color);
-      int16_t x = (rand() % bounds.size.w);
+      int16_t x = (rand() % width);
       //x = (x * x) / (bounds.size.w);
-      int16_t y = (rand() % bounds.size.h);
+      int16_t y = (rand() % height);
       //y = (y * y) / (bounds.size.h);
       int16_t r = (rand() % RADIUS) + 1;
       // (some random bumping left/down) + move it right and up
       // proportionally to its size. 
-      x = (rand() % 10)+((RADIUS-r+1) * x / RADIUS);
-      y = (rand() % 10)+((RADIUS-r+1) * y / RADIUS);
+      x = (rand() % 10)+((RADIUS-(2*r)+1) * x / RADIUS);
+      y = (rand() % 10)+((RADIUS-(2*r)+1) * y / RADIUS);
       // Animation: 
       x = x * s_animation_counter / ANIMATION_NORMALIZED_MAX;
       y = y * s_animation_counter / ANIMATION_NORMALIZED_MAX;
       graphics_fill_circle(ctx, GPoint(x, y), r);
+      #ifdef PBL_BW
+      graphics_context_set_fill_color(ctx, GColorBlack);
+      graphics_fill_circle(ctx, GPoint(x, y), r-1);
+      #endif
     } else {
       graphics_context_set_stroke_color(ctx, color);
-      int16_t x = (rand() % bounds.size.w);
-      int16_t y = (rand() % bounds.size.h);
+      int16_t x = (rand() % width);
+      int16_t y = (rand() % height);
       int16_t l = (rand() % RADIUS) + 3;
       // Animation:
-      x = x * s_animation_counter / ANIMATION_NORMALIZED_MAX;
-      y = y * s_animation_counter / ANIMATION_NORMALIZED_MAX;
-      graphics_draw_line(ctx, GPoint(x, y), GPoint(x-l, y-l));
+      int16_t shift = (x > y ? x : y) + l;
+      x = x + (-shift * (ANIMATION_NORMALIZED_MAX - s_animation_counter) / ANIMATION_NORMALIZED_MAX);
+      y = y + (-shift * (ANIMATION_NORMALIZED_MAX - s_animation_counter) / ANIMATION_NORMALIZED_MAX);
+      //x = x * s_animation_counter / ANIMATION_NORMALIZED_MAX;
+      //y = y * s_animation_counter / ANIMATION_NORMALIZED_MAX;
+
+      graphics_draw_line(ctx, GPoint(x, y), GPoint(x-x/l, y-y/l));
     }
     
   }
 }
 
 static void color_time_display() {
+  #ifdef PBL_COLOR
   srand(s_rand_seed);
   GColor rand_bg = random_color();
   GColor rand_fg = invert(rand_bg);
   text_layer_set_background_color(s_time_layer, rand_bg);
   text_layer_set_text_color(s_time_layer, rand_fg);
+  #else
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  #endif
 }
+
+
+
+
 static void canvas_anim_update(Animation *animation, const AnimationProgress progress) {
   s_animation_counter = progress;
   layer_mark_dirty(s_canvas_layer);
@@ -92,9 +120,14 @@ static const AnimationImplementation canvas_anim_impl = {
 };
 
 static void create_animation() { 
-  if (s_animation != NULL) {
-    animation_destroy(s_animation);
-  }
+  // This will be destroyed when it finishes apparently
+  static struct Animation *s_animation;
+  
+  // Except on APLITE
+  #ifdef PBL_PLATFORM_APLITE
+  if (s_animation) animation_destroy(s_animation);    
+  #endif
+
   s_animation = animation_create();
   animation_set_duration(s_animation, ANIMATION_DURATION);
   animation_set_curve(s_animation, AnimationCurveEaseOut);
@@ -102,6 +135,8 @@ static void create_animation() {
 
   animation_schedule(s_animation);
 }
+
+
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
@@ -118,9 +153,6 @@ static void main_window_load(Window *window) {
   color_time_display();
   text_layer_set_text(s_time_layer, "--:--");
   
-  // Create the animation
-  create_animation();
-  
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
@@ -130,8 +162,6 @@ static void main_window_load(Window *window) {
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy animation
-  animation_destroy(s_animation);
   // Destroy canvas
   layer_destroy(s_canvas_layer);
   // Destroy TextLayer
